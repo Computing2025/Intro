bf55588381a37055dd03ccdbead0c3aff6534eaa
Computing2025 <ozk8000@gmail.com>
2024-02-24 12:38:46 +0300

    Add project files.

---
+++ Intro/Business/CourseManager.cs
@@ -0,0 +1,49 @@
+﻿using Intro.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro.Business;
+
+public class CourseManager
+{
+    Course[] courses;
+
+    public CourseManager()
+    {
+        Course kurs1 = new()
+        {
+            Id = 1,
+            Name = ".NET MAUI",
+            Description = ".NET Multi-Platform",
+            Price = 0
+        };
+
+        Course kurs2 = new()
+        {
+            Id = 2,
+            Name = "Artificial Intelligence",
+            Description = "Making Software Great Again",
+            Price = 100
+        };
+
+        Course kurs3 = new()
+        {
+            Id = 3,
+            Name = "ASP.NET Web API",
+            Description = "Making Web Great Again",
+            Price = 500
+        };
+        
+       courses = [ kurs1, kurs2, kurs3];
+    }
+
+    public Course[] GetAll()
+    {
+        return courses;
+    }
+}
+
+
---
+++ Intro/Program.cs
@@ -0,0 +1,52 @@
+﻿
+
+
+using Intro.Entities;
+
+IndividualCustomer customer1 = new()
+{
+    Id = 1,
+    CustomerNumber = "232423",
+    FirstName = "John",
+    LastName = "Doe",
+    NationalIdentity = "3563634635463"
+};
+
+IndividualCustomer customer2 = new()
+{
+    Id = 2,
+    CustomerNumber = "93827",
+    FirstName = "Rose Mary",
+    LastName = "Solemnick",
+    NationalIdentity = "11239889732"
+};
+
+CorporateCustomer customer3 = new()
+{
+    Id=3,
+    Name = "Volkswagen",
+    CustomerNumber= "24323",
+    TaxNumber = "4252139"
+};
+
+CorporateCustomer customer4 = new()
+{
+    Id = 4,
+    Name = "Renault",
+    CustomerNumber = "94348",
+    TaxNumber = "2023939"
+};
+
+
+BaseCustomer[] customers = [ customer1, customer2, 
+                             customer3, customer4 ];
+
+foreach (BaseCustomer customer in customers)
+{
+  if (customer is IndividualCustomer)
+     Console.WriteLine(((IndividualCustomer)customer!).FirstName);
+  else
+     Console.WriteLine(((CorporateCustomer)customer!).TaxNumber);
+}
+
+
